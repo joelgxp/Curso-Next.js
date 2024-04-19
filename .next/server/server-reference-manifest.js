da6f8bdfa6a8358fed972f70e1602ba82173e9e0
Joel <joelvieirasouza@gmail.com>
2024-04-19 16:34:38 -0300

    update files

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uWc9slVaRPjaUsBpMAMG9vLHwVK/UO4fTwyKqktkhsY=\"\n}"
---
+++ .next/static/webpack/app/page.424d10c055b0fa1a.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CWorkSpace%5C%5CCurso-Next.js%5C%5Ccurso%5C%5Capp%5C%5Cpage.module.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!":
+/*!************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CWorkSpace%5C%5CCurso-Next.js%5C%5Ccurso%5C%5Capp%5C%5Cpage.module.css%22%2C%22ids%22%3A%5B%5D%7D&server=false! ***!
+  \************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./app/page.module.css */ \"(app-pages-browser)/./app/page.module.css\", 23));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyQyUzQSU1QyU1Q1dvcmtTcGFjZSU1QyU1Q0N1cnNvLU5leHQuanMlNUMlNUNjdXJzbyU1QyU1Q2FwcCU1QyU1Q3BhZ2UubW9kdWxlLmNzcyUyMiUyQyUyMmlkcyUyMiUzQSU1QiU1RCU3RCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLDBLQUE4RiIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvP2FmZWIiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJDOlxcXFxXb3JrU3BhY2VcXFxcQ3Vyc28tTmV4dC5qc1xcXFxjdXJzb1xcXFxhcHBcXFxccGFnZS5tb2R1bGUuY3NzXCIpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22C%3A%5C%5CWorkSpace%5C%5CCurso-Next.js%5C%5Ccurso%5C%5Capp%5C%5Cpage.module.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
