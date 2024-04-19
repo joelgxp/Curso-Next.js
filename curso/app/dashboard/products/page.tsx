4e3087ea077f4756ddc3f388b4e0de62d5cb0ea2
Joel <joelvieirasouza@gmail.com>
2024-04-19 12:57:06 -0300

    feat: dashboard

---
+++ curso/app/dashboard/products/page.tsx
@@ -0,0 +1,10 @@
+import styles from "../../page.module.css"
+
+export default function Products
+() {
+    return (
+        <main className={styles.main}>
+            <h1>Products</h1>  
+        </main>        
+    );
+}
---
+++ curso/app/dashboard/users/page.tsx
@@ -0,0 +1,12 @@
+import styles from "../../page.module.css"
+
+export default function Users
+() {
+    return (
+        <main className={styles.main}>
+
+            <h1>Users</h1>  
+
+        </main>        
+    );
+}
